2b76e86a88676f7ec5afb6c2cfdcd6fcbe75ad42
shi-cong <shi_cong@icloud.com>
2017-10-26 12:44:10 +0800

    fix sth.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import codecs
+from setuptools import setup
+
+setup(
+    name="stayawake",
+    version="2.8.2",
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    packages=['stayawake', 'stayawake.crypto'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: Proxy Servers',
+    ],
+)
